d482e9aa603237b73dafb78a94e64d04a4e2caff
BogdanZanozdra <107503960+BogdanZanozdra@users.noreply.github.com>
2023-05-31 16:34:12 +0300

    Add files via upload

---
+++ DZ_3.java
@@ -0,0 +1,25 @@
+package DZ.DZ_3;
+
+public class DZ_3 {
+    public static void main(String[] args) {
+    }
+
+    public class Node{
+        int value;
+        Node next;
+        Node previous;
+    }
+    Node head;
+    Node tail;
+
+    public void reverse(Node currentNode, Node previousNode){
+        if (currentNode.next == null){
+            head = currentNode;
+        }
+        else {
+            reverse(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+        previousNode.next = null;
+    }
+}
